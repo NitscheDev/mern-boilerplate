896d687a6312fb3fe41ca5698acd5280323bee84
Rasmus Nitsche <nitschedev@gmail.com>
2022-03-31 00:22:47 +0200

    Initialize Application

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Logo from './logo.png'
+
+function App() {
+    return (
+        <div className="app">
+            <img src={Logo} alt="React.js" className="logo" />
+        </div>
+    )
+}
+
+export default App
