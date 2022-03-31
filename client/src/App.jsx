8e615e7a9f3745feaa713641891caf9f0d1aaa04
Rasmus Nitsche <nitschedev@gmail.com>
2022-03-31 15:31:10 +0200

    cleanup code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,20 @@ import Logo from './logo.png'
 import { useEffect } from 'react'
 
 function App() {
+    //fetch backend api on mount -> check console
     useEffect(() => {
-        fetch('/api').then(data => {
-            data.json().then(json => {
+        const fetchApi = async () => {
+            try {
+                const data = await fetch('/api')
+                const json = await data.json()
                 console.log(json)
-            }) 
-        }).catch(error => {
-            console.log(error)
-        })
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchApi()
     })
+
     return (
         <div className="app">
             <img src={Logo} alt="React.js" className="logo" />
