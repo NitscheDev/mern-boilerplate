5ead8c6e5296cfad2b347bceb67d0aa2d40540a9
Rasmus Nitsche <nitschedev@gmail.com>
2022-03-31 15:25:30 +0200

    update client & server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import './App.css'
 import Logo from './logo.png'
+import { useEffect } from 'react'
 
 function App() {
+    useEffect(() => {
+        fetch('/api').then(data => {
+            data.json().then(json => {
+                console.log(json)
+            }) 
+        }).catch(error => {
+            console.log(error)
+        })
+    })
     return (
         <div className="app">
             <img src={Logo} alt="React.js" className="logo" />
--- server/index.js
+++ server/index.js
@@ -2,12 +2,23 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
-const cors = require('cors')
+const path = require('path')
 
+//Middlewere's
 app.use(express.json())
 app.use(express.urlencoded())
-app.use(cors())
 
+//Api Routes
 app.use('/api', require('./routes/api.route'))
 
+//Serve frontend - In production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+    //catch all(*) routes after the api routes and serve the client index.html
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'))
+    })
+}
+
+//Start express server
 app.listen(port, () => console.log(`Server alive at port ${port}`))
